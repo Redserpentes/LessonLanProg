f7c70873b16cc1f61c13a2cf582bd2bfabf96419
RedSerpentes <redserpentes@yane.ru>
2022-12-23 01:01:26 +0300

    оптимизировали папки

---
+++ task004/task006/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Clear();
+
+int[] array = { 4, -3, 7 };
+
+int index = 0;
+int n = array.Length;
+
+while (index < n)
+
+{
+    if(array[index] % 2 ==0)
+    {
+        Console.Write($"{array[index]} - четное");
+        Console.WriteLine();
+    }
+    
+    else
+    {
+        Console.WriteLine($"{array[index]} - нечетное");
+    } 
+index++;
+}
+
+
+
+
