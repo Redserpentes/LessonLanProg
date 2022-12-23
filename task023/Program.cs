beee2c413be9abbaefd1e6da978373787e0e7095
RedSerpentes <redserpentes@yane.ru>
2022-12-23 12:53:58 +0300

    решена задача 23

--- task023/Program.cs
+++ task023/Program.cs
@@ -5,3 +5,22 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+
+Console.Clear();
+
+int n;
+
+Console.Write($"Введите число N: ");
+
+Console.WriteLine();
+
+Console.Write("N= ");
+int.TryParse(Console.ReadLine()!, out n);
+
+Console.Write($"Квадрат чисел от 1 до {n}: ");
+
+
+for (int i = 1; i <= n; i++)
+    Console.Write($"{Math.Pow(i, 3)}, ");
+
+
