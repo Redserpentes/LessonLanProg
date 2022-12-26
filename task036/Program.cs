5acacf1c3b20dc7a4ac02a97ff1521dd802cd7af
RedSerpentes <redserpentes@yane.ru>
2022-12-26 23:34:35 +0300

    решена задача 36

--- task036/Program.cs
+++ task036/Program.cs
@@ -7,3 +7,27 @@
 [-4, -6, 89, 6] -> 0
 */
 
+Console.Clear();
+
+Console.WriteLine("Задайте велечину массива: ");
+int n = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Задайте максимальное число массива: ");
+int m = Convert.ToInt32( Console.ReadLine());
+
+int[] array = new int[n];
+Console.Write($"Массив из {n} элементо: ");
+int j=0;
+
+for (int i =0; i < n; i++)
+{
+    array[i] = new Random().Next(0, m);
+    Console.Write($"{array[i]}|");
+         if (i % 2 == 0)
+    {
+        j += array[i];
+    }
+
+}
+
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел на четных позициях данного массиве: {j} ");
